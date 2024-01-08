49683a7fb892835dd991dbccf55bf7f78dd2b9c9
kevinwen <kevinwenwork@qq.com>
2024-01-08 10:32:44 +0800

    优化屏幕尺寸小的显示效果

--- example/App.vue
+++ example/App.vue
@@ -1,14 +1,14 @@
 <template>
-<div>
-  <vue-datepicker-local v-model="timeRange" format="YYYY-MM-DD HH:mm:ss" :disabled-date="disabledDate" @input="change"/><br/>
+<div style="text-align: center;">
+  <!-- <vue-datepicker-local v-model="timeRange" format="YYYY-MM-DD HH:mm:ss" :disabled-date="disabledDate" @input="change"/><br/>
   <vue-datepicker-local v-model="timeRange" format="YYYY-MM-DD HH:mm:ss" :disabled-date="disabledDate" :local="local" range-separator="至"/><br/>
   <vue-datepicker-local v-model="timeRange" format="YYYY-MM-DD HH:mm:ss" :disabled-date="disabledDate" :local="local" show-buttons @confirm="selectedDate" clearable @cancel="cancel" @clear="clear"/><br/>
   <vue-datepicker-local v-model="time" format="YYYY-MMM-DD" :local="local" /><br/>
   <vue-datepicker-local v-model="time" format="YYYY-MM" show-buttons @confirm="selectedDate"/><br/>
-  <vue-datepicker-local v-model="time" format="YYYY"/><br/>
+  <vue-datepicker-local v-model="time" format="YYYY"/><br/> -->
   <vue-datepicker-local v-model="empty" format="YYYY-MM-DD HH:mm:ss" clearable placeholder="select date"/><br/>
-  <vue-datepicker-local v-model="now" disabled/><br/>
-  <vue-datepicker-local v-model="timeRange"/><br/>
+  <vue-datepicker-local v-model="now"/><br/>
+  <!-- <vue-datepicker-local v-model="timeRange"/><br/> -->
 </div>
 </template>
 
